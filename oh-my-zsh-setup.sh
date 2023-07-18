11ad521870cfc8177fff35622bded4ae202f52a3
Gradrix <gradrix@main.lan>
2023-07-18 21:56:11 +0000

    some update

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -4,12 +4,10 @@ echo "Installing zsh"
 sudo apt install zsh
 
 echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf /home/gradrix/.oh-my-zsh/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ~/.oh-my-zsh 
 
 echo "Installing powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "Updating .zshrc values:"
-sed -i 's/^ZSH_THEME\=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-sed -i 's/alias p\=.*/alias p="python3"/' ~/.zshrc
-sed -i 's/alias temp\=.*/alias temp="\/opt\/vc\/bin\/vcgencmd measure_temp"/' ~/.zshrc
+source ~/.zshrc
