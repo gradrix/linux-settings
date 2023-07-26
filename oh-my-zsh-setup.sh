deb53d1c23d23822c31f2f8b3fee0be43f0bdb48
Gražvydas Jučius <grazjuc@gmail.com>
2023-07-26 12:41:53 +0300

    power level setup fix

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -7,7 +7,4 @@ echo "Installing Oh My Zsh"
 rm -rf /home/gradrix/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ~/.oh-my-zsh 
 
-echo "Installing powerlevel10k"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 source ~/.zshrc
---
+++ powerlevel10k-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+source ~/.zshrc
